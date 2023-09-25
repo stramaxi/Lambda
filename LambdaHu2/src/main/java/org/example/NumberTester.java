a3dacd96a8d7ef322d75a871a7fa409f74831680
stramaxi <maxistranzinger@gmail.com>
2023-09-25 12:28:16 +0200

    NumbTest Interface

--- LambdaHu2/src/main/java/org/example/NumberTester.java
+++ LambdaHu2/src/main/java/org/example/NumberTester.java
@@ -1,4 +1,17 @@
 package org.example;
 
-public class NumberTester {
+public class NumberTester
+{
+    private String fileName;
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+    public void setOddEvenTester(NumberTest oddTester)
+    {
+
+    }
+
+
+
 }
