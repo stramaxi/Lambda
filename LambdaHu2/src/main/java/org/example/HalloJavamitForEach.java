3ac96cc40b9e6c2ac2dec0b3afd2b18000b4cfcc
stramaxi <maxistranzinger@gmail.com>
2023-09-25 12:01:08 +0200

    klasse

---
+++ LambdaHu2/src/main/java/org/example/HalloJavamitForEach.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import java.util.List;
+
+public class HalloJavamitForEach {
+
+
+    List<String> list;
+
+    public HalloJavamitForEach() {
+        
+        list.add("1");
+        list.add("2");
+    }
+}
